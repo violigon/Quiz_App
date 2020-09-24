3358a7f6a9ceb2c57c25670fc85d4550973f9a5e
Vinícius Oliveira Gonçalves <viniciusoligon@outlook.com>
2020-09-24 16:35:07 -0300

    style do question card

---
+++ my-app/src/components/QuestionCard.styles.ts
@@ -0,0 +1,11 @@
+import styled from 'styled-components';
+
+export const Wrapper = styled.div`
+    max-width: 1100px;
+    background: #ebfeff;
+    border-radius:10px;
+    border: 2pc solid #0085a3;
+    padding: 20px;
+    box-shadow: 0px 5px 10px rgba(0,0,0,0.25);
+
+`
