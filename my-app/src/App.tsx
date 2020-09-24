7d7f0db829cb838d1e4405d3fc85c610041c5980
Vinícius Oliveira Gonçalves <viniciusoligon@outlook.com>
2020-09-24 12:07:42 -0300

    funçao start

--- my-app/src/App.tsx
+++ my-app/src/App.tsx
@@ -3,22 +3,41 @@ import { fetchQuizQuestions } from './API';
 //Components
 import QuestionCard from './components/QuestionCard';
 //Types
-import { Difficulty } from './API';
+import { QuestionState, Difficulty } from './API';
+
+type AnswerObject = {
+  question: string;
+  answer: string;
+  correct: boolean;
+  correctAnswer: string;
+}
 
 const TOTAL_QUESTIONS = 10;
 const App = () => {
   const [loading, setLoading] = useState(false);
-  const [questions, setQuestions] = useState([]);
+  const [questions, setQuestions] = useState<QuestionState[]>([]);
   const [number, setNumber] = useState(0);
-  const [userAnswers, setUserAnswers] = useState([]);
+  const [userAnswers, setUserAnswers] = useState<AnswerObject[]>([]);
   const [score, setScore] = useState(0);
   const [gameOver, setGameOver] = useState(true);
 
   console.log(fetchQuizQuestions(TOTAL_QUESTIONS, Difficulty.EASY))
 
-  const startTrivia = async () => { 
+  const startTrivia = async () => {
+    setLoading(true);
+    setGameOver(false);
 
-  }
+    const newQuestions = await fetchQuizQuestions(
+      TOTAL_QUESTIONS,
+      Difficulty.EASY
+    );
+
+    setQuestions(newQuestions);
+    setScore(0);
+    setUserAnswers([]);
+    setNumber(0);
+    setLoading(false);
+  };
   const checkAnswer = (e: React.MouseEvent<HTMLButtonElement>) => {
     
   }
