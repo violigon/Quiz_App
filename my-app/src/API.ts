48abed30a91b9184f462bfca9385fcc3f0ea69af
Vinícius Oliveira Gonçalves <viniciusoligon@outlook.com>
2020-09-24 11:57:09 -0300

    importando a funçao fetch para o App
    update no Utils e API

--- my-app/src/API.ts
+++ my-app/src/API.ts
@@ -1,4 +1,14 @@
+import { shuffleArray } from './utils';
+export type Question = {
+  category: string;
+  correct_answer: string;
+  difficulty: string;
+  incorrect_answers: string[];
+  question: string[];
+  type: string;
+}
 
+export type QuestionState = Question & { answers: string[] };
 
 export enum Difficulty{
   EASY = "easy",
@@ -7,8 +17,18 @@ export enum Difficulty{
 
 }
 
-export const fetchQuizQuestions = async (amount: number, difficulty: Difficulty) => {
+export const fetchQuizQuestions = async (
+  amount: number,
+  difficulty: Difficulty
+) => {
   const endpoint = `https://opentdb.com/api.php?amount=${amount}&difficulty${difficulty}&type=multiple`;
   const data = await (await fetch(endpoint)).json();
-  console.log(data);
+  return data.results.map((question: Question) => ({
+    ...question,
+    answers: shuffleArray([
+      ...question.incorrect_answers,
+      question.correct_answer,
+    ])
+  }
+  ))
 }
--- my-app/src/App.tsx
+++ my-app/src/App.tsx
@@ -1,6 +1,9 @@
-import React, {useState} from 'react';
+import React, { useState } from 'react';
+import { fetchQuizQuestions } from './API';
 //Components
 import QuestionCard from './components/QuestionCard';
+//Types
+import { Difficulty } from './API';
 
 const TOTAL_QUESTIONS = 10;
 const App = () => {
@@ -10,6 +13,8 @@ const App = () => {
   const [userAnswers, setUserAnswers] = useState([]);
   const [score, setScore] = useState(0);
   const [gameOver, setGameOver] = useState(true);
+
+  console.log(fetchQuizQuestions(TOTAL_QUESTIONS, Difficulty.EASY))
 
   const startTrivia = async () => { 
 
@@ -27,14 +32,14 @@ const App = () => {
       <button className="start" onClick={startTrivia}>Começar</button>
       <p className="score">Pontos:</p>
       <p>Carregando questões... </p>
-      <QuestionCard
+      {/* <QuestionCard
         questionNr={number + 1}
         totalQuestions={TOTAL_QUESTIONS}
         question={questions[number].question}
         answers={questions[number].answers}
         userAnswer={userAnswers ? userAnswers[number] : undefined}
         callback={checkAnswer}
-      />
+      /> */}
       <button className="next" onClick={nextQuestion}>
         Próxima Questão
       </button>
