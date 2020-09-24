bd94f96bd5ed062f3648c6c8d6ed144972e93c4d
Vinícius Oliveira Gonçalves <viniciusoligon@outlook.com>
2020-09-24 10:12:18 -0300

    API inserida, random e props criado

--- my-app/src/App.tsx
+++ my-app/src/App.tsx
@@ -1,9 +1,31 @@
 import React from 'react';
+//Components
+import QuestionCard from './components/QuestionCard';
 
+const App = () => {
 
-function App() {
+  const startTrivia = async () => { 
+
+  }
+  const checkAnswer = (e: React.MouseEvent<HTMLButtonElement>) => {
+    
+  }
+
+  const nextQuestion = () => {
+    
+  }
+
   return (
-    <div className="App">Quiz</div>
+    <div className='App'>
+      <h1>REACT QUIZ</h1>
+      <button className="start" onClick={startTrivia}>Começar</button>
+      <p className="score">Pontos:</p>
+      <p>Carregando questões... </p>
+      <QuestionCard />
+      <button className="next" onClick={nextQuestion}>
+        Próxima Questão
+      </button>
+    </div>
   );
 }
 
--- my-app/src/components/QuestionCard.tsx
+++ my-app/src/components/QuestionCard.tsx
@@ -1,5 +1,22 @@
 import React from 'react';
 
-const QuestionCard = () => <div>Cartão de Questões</div>;
+type Props = {
+  question: string;
+  answer: string[];
+  callback: any;
+  userAnswer: string;
+  questionNr: number;
+  totalQuestions: number;
+}
+const QuestionCard: React.FC<Props> = ({
+  question,
+  answer,
+  callback,
+  userAnswer,
+  questionNr,
+  totalQuestions,
+}) => (
+  <div>Cartão de Questões</div>
+);
 
 export default QuestionCard;
